9ff82fc997c0491e68905022d602a41d8fef9f31
Whisker17 <yukaijun123456@163.com>
2022-05-20 18:31:24 +0800

    add local search

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('/home/whisker/Docusaurus-feature-test/node_modules/infima/dist/css/default/default.css'),
+  require('/home/whisker/Docusaurus-feature-test/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/whisker/Docusaurus-feature-test/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/home/whisker/Docusaurus-feature-test/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/whisker/Docusaurus-feature-test/src/css/custom.css'),
+];
